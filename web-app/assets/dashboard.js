4b44fe3f7649b47fa771b88fe6cfe37efff0fad7
rikesh7 <acharyarikesh7@gmail.com>
2023-01-23 12:01:59 +0545

    td value assigned

--- web-app/assets/dashboard.js
+++ web-app/assets/dashboard.js
@@ -3,8 +3,19 @@
 const relay = document.querySelector('.relay');
 const info = document.querySelector('.info');
 const restart = document.querySelector('.restart');
+const table = document.querySelector('table');
 let reload_dashboard_flag = 0;
 let reload_relay_flag = 0;
+td1 = document.querySelector('.cm');
+td2 = document.querySelector('.ci');
+td3 = document.querySelector('.cc');
+td4 = document.querySelector('.fs');
+td5 = document.querySelector('.hs');
+td6 = document.querySelector('.fd');
+td7 = document.querySelector('.fi');
+td8 = document.querySelector('.fh');
+td9 = document.querySelector('.bc');
+td10 = document.querySelector('.ct');
 //relay button onclick function
 relay.addEventListener('click', function () {
   fetch('http://192.168.1.100/relay')
@@ -33,6 +44,17 @@ info.addEventListener('click', async function (e) {
     console.log(json_data);
     // console.log('Approve Status : ', json_data.approve);
     if (json_data.approve == 1) {
+      td1 = json_data.CHIP_MODEL;
+      td2 = json_data.CHIP_ID_MAC;
+      td3 = json_data.CHIP_CORES;
+      td4 = json_data.FLASH_SIZE;
+      td5 = json_data.HEAP_SIZE;
+      td6 = json_data.FREE_DRAM;
+      td7 = json_data.FREE_IRAM;
+      td8 = json_data.FREE_HEAP;
+      td9 = json_data.BOOT_COUNT;
+      td10 = json_data.COMPILE_TIME;
+
       // info.classList.add('hidden');
       // alert('Restarting the system...');
       window.location.replace('http://192.168.1.100/info');
